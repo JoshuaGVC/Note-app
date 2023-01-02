a4338eb8b3e9154fa3fce6e6c57e14ac8dc59ac6
joshuaGVC <josvc2394@gmail.com>
2023-01-02 17:03:10 -0500

    WIP(Home):selected

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,25 @@ import { TAction } from "./components/Card/Card.d";
 
 const App = () => {
   const [cardsList, setCardsList] = useState<ICardApp[]>([]);
-  const [isSelected, setIsSelected] = useState(true);
 
   const addCard = () => {
+    const prevList = cardsList.map((item) => {
+      return {
+        ...item,
+        selected: false,
+      };
+    });
+
     const newCard = {
       id: crypto.randomUUID(),
       title: "Una simple nota",
       paragraph:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nemo labore aliquam doloremque exercitationem necessitatibus voluptates ex quia porro iure eius sequi, consequuntur veniam, libero ullam vero iste. Vero, maiores.",
       date: moment().format("dddd, Do of MMMM  YYYY, h:mm:ss a"),
-      selected: isSelected,
+      selected: true,
     };
 
-    const addedNote = (prev: ICardApp[]) => {
-      return [newCard, ...prev];
-    };
-
-    setCardsList(addedNote);
+    setCardsList([newCard, ...prevList]);
   };
 
   const deleteNote = (id: string) => {
